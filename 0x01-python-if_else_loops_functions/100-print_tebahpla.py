68418e4ed0a18d2409999395bb301a9324a21a52
Stephen Oba <obastepheno@gmail.com>
2022-08-23 21:44:15 +0100

    used string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for char in reversed(range(97, 123)):
     if char % 2 == 1:
-        char = char - 32
-    print(chr(char), end="")
+        char -= 32
+    print("{}".format(chr(char)), end="")
