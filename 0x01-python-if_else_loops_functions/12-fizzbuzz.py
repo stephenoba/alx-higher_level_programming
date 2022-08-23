55fe7a90289e670993cb062779f4068482b60d59
Stephen Oba <obastepheno@gmail.com>
2022-08-23 15:48:28 +0100

    fizzbuzz function

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            i = "FizzBuzz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        elif i % 3 == 0:
+            i = "Fizz"
+        print("{}".format(i), end=" ")
+
+fizzbuzz()
