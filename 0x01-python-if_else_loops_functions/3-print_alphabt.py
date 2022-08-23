aafafc1c5fd6eef93be1fecd3b69257c551a2f06
Stephen Oba <obastepheno@gmail.com>
2022-08-23 14:30:02 +0100

    fixed requirement error with task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
-"""
-prints the ASCII alphabet except 'e' and 'q',
-in lowercase, not followed by a new line
-"""
 for char in range(97, 123):
     if chr(char) is not 'e' and chr(char) is not 'q':
         print("{}".format(chr(char)), end="")
