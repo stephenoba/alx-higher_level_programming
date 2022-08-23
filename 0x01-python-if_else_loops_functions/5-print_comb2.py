46b08744b12bbf12374b7e4749b1dd46c7564f71
Stephen Oba <obastepheno@gmail.com>
2022-08-23 14:27:41 +0100

    nitpick

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -5,7 +5,7 @@ prints numbers from 0 - 99 seperated by ', '
 with padding
 """
 for num in range(100):
-    if num is 99:
+    if num == 99:
         print("{}".format(num))
     else:
         print("{:02}".format(num), end=", ")
