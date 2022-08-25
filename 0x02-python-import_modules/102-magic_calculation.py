14a7caca2842a1fd422d039d6b659347dbf62fe7
Stephen Oba <obastepheno@gmail.com>
2022-08-25 21:52:14 +0100

    fixed requirement error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from magi_calculation_102 import add, sub
 
 def magic_calculation(a, b):
+    from magi_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
