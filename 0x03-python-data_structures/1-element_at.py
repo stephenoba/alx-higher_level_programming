00da6fa55f6d0e4b20ff7bb26e0d448660094a74
Stephen Oba <obastepheno@gmail.com>
2022-08-27 13:11:03 +0100

    fixed pycodestyle error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,7 @@ def element_at(my_list, idx):
     get element at index
     """
     list_len = len(my_list)
-    
+
     if idx > 0 or idx < list_len:
         for i in range(len(my_list)):
             if i == idx:
