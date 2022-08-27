82125984153f74feb26afe41cfded976cf702b9e
Stephen Oba <obastepheno@gmail.com>
2022-08-27 12:52:45 +0100

    formated output

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
     """
     Prints each integer in a list.
     """
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
