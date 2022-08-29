fb1d26e44760d68b616d9a87e3bccdbc5d3fd677
Stephen Oba <obastepheno@gmail.com>
2022-08-29 07:11:24 +0000

    updated file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,11 +3,7 @@
 
 def new_in_list(my_list, idx, element):
     """updates value in copy of a list"""
-    new_list = my_list.copy()
-    list_len = len(new_list)
+    new_list = [x for x in my_list]
     if idx > 0 and idx < list_len:
-        for i in range(list_len):
-            if i == idx:
-                new_list[i] = element
-                break
+        new_list[idx] = element
     return new_list
