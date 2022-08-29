a945945a1de469aa6a166c6e307933ce49fca222
Stephen Oba <obastepheno@gmail.com>
2022-08-29 06:50:39 +0000

    fixed pycodestyle requirement error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,5 @@ def print_reversed_list_integer(my_list=[]):
     """print a list reversed"""
     list_len = len(my_list)
     while list_len != 0:
-        list_len-=1
+        list_len -= 1
         print("{:d}".format(my_list[list_len]))
