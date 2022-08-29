50f33be8bf44b570393e702311f75c5e086fd38b
Stephen Oba <obastepheno@gmail.com>
2022-08-29 12:42:24 +0000

    refractored function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,14 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """adds two tuples"""
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
-            tuple_a = [0, 0]
+            tuple_a = (0, 0)
         else:
-            tuple_a = list(tuple_a)
-            tuple_a.append(0)
+            tuple_a = (tuple_a[0], 0)
     if len(tuple_b) < 2:
         if len(tuple_b) == 0:
-            tuple_b = [0, 0]
+            tuple_b = (0, 0)
         else:
-            tuple_b = list(tuple_b)
-            tuple_b.append(0)
-    return tuple([sum(x) for x in zip(tuple_a, tuple_b)])
+            tuple_b = (tuple_b[0], 0)
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_a[1]
