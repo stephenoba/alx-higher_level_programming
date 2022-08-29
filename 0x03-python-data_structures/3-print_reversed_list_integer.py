5a76b0fd0b55d5a56a9725948dad0e0440ca41f4
Stephen Oba <obastepheno@gmail.com>
2022-08-29 05:53:44 +0000

    added function to print list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 3-print_reversed_list_integer.py
+
+def print_reversed_list_integer(my_list=[]):
+    """print a list reversed"""
+    list_len = len(my_list)
+    while list_len != 0:
+        list_len-=1
+        print("{:d}".format(my_list[list_len]))
