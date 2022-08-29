9b3debc87fbc0bb702f1e64b131d091703db51ec
Stephen Oba <obastepheno@gmail.com>
2022-08-29 12:38:47 +0000

    refractored function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 # 7-add_tuple.py
 
-
-def pad_tuple(tuple_x):
-    """adds zeros to tuple with length less than 2"""
-    tup_to_list = list(tuple_x)
-    i = 2  # max length
-    while i != len(tuple_x):
-        i -= 1
-        tup_to_list.append(0)
-    return tup_to_list
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
     """adds two tuples"""
-    tuple_a = pad_tuple(tuple_a)
-    tuple_b = pad_tuple(tuple_b)
-    total = [sum(x) for x in zip(tuple_a, tuple_b)]
-    return tuple(total)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = [0, 0]
+        else:
+            tuple_a = list(tuple_a)
+            tuple_a.append(0)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = [0, 0]
+        else:
+            tuple_b = list(tuple_b)
+            tuple_b.append(0)
+    return tuple([sum(x) for x in zip(tuple_a, tuple_b)])
