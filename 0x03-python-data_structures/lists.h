d8227b7d83a3946bb6cd2135a5a03b408521321e
Stephen Oba <obastepheno@gmail.com>
2022-08-30 02:48:28 +0100

    added prototypes to lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -20,6 +20,8 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+listint_t *add_to_start(listint_t **head, int n);
+listint_t *reverse_list(listint_t *head, size_t mid);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
