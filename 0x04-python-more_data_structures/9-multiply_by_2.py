903857194851c6a0a50ba0b7c6b05a5b6c9d0e38
Stephen Oba <obastepheno@gmail.com>
2022-08-31 15:23:48 +0100

    Fixed pycodestyle error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,8 @@
 # 9-multiply_by_2.py
 
 def multiply_by_2(a_dictionary):
+    """returns a new dictionary with all values multiplied by two"""
     new_dictionary = a_dictionary.copy()
     for i in new_dictionary.keys():
-        new_dictionary[i]*=2
+        new_dictionary[i] *= 2
     return new_dictionary
