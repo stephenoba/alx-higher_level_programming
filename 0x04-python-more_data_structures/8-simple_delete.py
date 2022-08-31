563860c8cf637bcc7550449ecfd4f9ce4f53a8f6
Stephen Oba <obastepheno@gmail.com>
2022-08-31 14:57:54 +0100

    Fixed errored output

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,6 +3,6 @@
 
 def simple_delete(a_dictionary, key=""):
     """deletes a key in a dictionary"""
-    if key:
+    if key or (key in a_dictionary.keys():
         del a_dictionary[key]
-    return a_dictionary.copy()
+    return a_dictionary
