250498fd276e82b75f38c5a0510ec2d92cf28177
Stephen Oba <obastepheno@gmail.com>
2022-09-13 00:20:05 +0100

    Add variable count

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,6 +3,8 @@
 # Stephen Oba <obastepheno@gmail.com>
 
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
