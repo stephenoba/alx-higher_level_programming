0131e077831bc9b4f8d19f7728a424bad5ab2bd8
Stephen Oba <obastepheno@gmail.com>
2022-09-12 12:20:16 +0100

    Add check for type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,6 @@ def safe_print_integer(value):
     flag = True
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         flag = False
     return flag
