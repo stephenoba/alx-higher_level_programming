3e7116902bae610c94b512e7f250809d926b0742
Stephen Oba <obastepheno@gmail.com>
2022-09-13 00:18:34 +0100

    remove extra except block

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,18 +3,11 @@
 # Stephen Oba <obastepheno@gmail.com>
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    if x < 0:
-        return count
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            continue
-        except IndexError:
-            break
-        except Exception:
             continue
     print("")
     return count
