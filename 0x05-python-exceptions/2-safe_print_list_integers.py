6a408048ea61417e025db983c29d5662ceebc8ad
Stephen Oba <obastepheno@gmail.com>
2022-09-13 00:16:18 +0100

    add except block to handle uncaught errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -14,5 +14,7 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
         except IndexError:
             break
+        except Exception:
+            continue
     print("")
     return count
