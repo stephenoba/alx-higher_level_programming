9886ec83a79b38e0e955aab848bc23dfd045572b
Stephen Oba <obastepheno@gmail.com>
2022-09-12 09:35:45 +0100

    Create function safe_print_integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-safe_print_integer.py
+# Stephen Oba <obastepheno@gmail.com>
+
+def safe_print_integer(value):
+    flag = True
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        flag = False
+    return flag
