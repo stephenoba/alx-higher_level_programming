c22eef7181cf27d37fe6c30e02692a58cf5afe78
Stephen Oba <obastepheno@gmail.com>
2022-09-12 10:46:06 +0100

    Create function raise_exception_msg

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Stephen Oba <obastepheno@gmail.com>
+
+def raise_exception_msg(message=""):
+    """Raise a Name exception"""
+    raise NameError(message)
