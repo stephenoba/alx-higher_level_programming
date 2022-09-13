5a82805b9a25b425d7530f87e143ad54b2356649
Stephen Oba <obastepheno@gmail.com>
2022-09-13 13:45:06 +0100

    Create an empty square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-square.py
+# Stephen Oba <obastepheno@gmail.com>
+"""
+Module provides a class for cresting Square objects
+"""
+
+class Square:
+    """
+    Empty Square class that defines a square
+    """
+    pass
