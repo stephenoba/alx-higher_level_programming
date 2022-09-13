6e80146ac4ad1ba79ebc611b74d08de8f8d2b316
Stephen Oba <obastepheno@gmail.com>
2022-09-13 13:59:39 +0100

    Add a private size attribute to class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+# Stephen Oba <obastepheno@gmail.com>
+"""
+Module provides a class for cresting Square objects
+"""
+
+
+class Square:
+    """
+    Square class that defines a square with a size
+    """
+    def __init__(self, size):
+        """Initialize object"""
+        self.__size = size
