cec3fe0f1694df02463c02b90a55ecd7a46a4c71
Stephen Oba <obastepheno@gmail.com>
2022-09-13 14:12:03 +0100

    Add check on user supplied size value

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 2-square.py
+# Stephen Oba <obastepheno@gmail.com>
+"""
+Module provides a class for cresting Square objects
+"""
+
+
+class Square:
+    """
+    Square class that defines a square with a size
+    """
+    def __init__(self, size=0):
+        """Initialize object"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
