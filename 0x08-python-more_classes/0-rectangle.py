1eb142bd6904f4e7ab73df2f7d2f9650c11e0d5b
Stephen Oba <obastepheno@gmail.com>
2022-09-19 17:13:32 +0100

    Add Rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Stephen Oba <obastepheno@gmail.com>
+"""
+Module defines a Rectangle Class
+"""
+
+class Rectangle:
+    """
+    Class used to represent a rectangle
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    None
+    """
+    pass
