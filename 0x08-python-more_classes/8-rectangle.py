7ee4235ca05eb03e623163b15d30e783ba77c65b
Stephen Oba <obastepheno@gmail.com>
2022-09-19 22:08:58 +0100

    Fix typo in error messsage

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -114,7 +114,7 @@ class Rectangle:
             TypeError if rect_1 or rect_2 are not Rectangle instances
         """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instane of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         rect_1_area = rect_1.area()
