7d715971ecb8e4b99cda317d1181fd95f63a4e03
Stephen Oba <obastepheno@gmail.com>
2022-09-20 14:52:14 +0100

    Add function copy_list

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 19-copy_list.py
+# Stephen Oba <obastepheno@gmail.com>
+
+def copy_list(l):
+    """
+    Copy a list
+
+    Args:
+        l (list): List to copy
+
+    Return:
+        A new list
+    """
+    return l[:]
