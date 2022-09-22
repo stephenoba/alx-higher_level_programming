479bf0b5aa751d2838e6b1d67b7e9e1d5d6fd1d9
Stephen Oba <obastepheno@gmail.com>
2022-09-22 08:15:11 +0100

    Fix pycodestyle warning

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,7 @@
 
 def text_indentation(text):
     """
-    prints a text with 2 new lines after each of these characters: `.`, 
+    prints a text with 2 new lines after each of these characters: `.`,
     `?` and `:`
 
     Args:
