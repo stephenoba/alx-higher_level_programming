954b9839a0ae866643205a58ccb27ca7b4d5f9e1
Stephen Oba <obastepheno@gmail.com>
2022-09-26 14:55:29 +0100

    Add class Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 10-square.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module provides definition of a Square class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class representation of a Square
+    """
+    def __init__(self, size):
+        """Initialize a Square object
+
+        Args:
+            size (int): width and height of square
+
+        Return:
+            None
+        """
+        super().__init__(size, size)
+        self.__size = size
