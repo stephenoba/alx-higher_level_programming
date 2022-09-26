a5e25c3466e37d9e5731d6309c304ea8da9716d2
Stephen Oba <obastepheno@gmail.com>
2022-09-26 11:40:12 +0100

    Add method area to BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module contains defintion of class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry operations
+    """
+    def area(self):
+        """Cumpute area
+        """
+        raise Exception("area() is not implemented")
