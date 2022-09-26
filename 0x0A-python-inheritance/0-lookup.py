715c6baa069dea1309fecc4220bbbd4777ee003c
Stephen Oba <obastepheno@gmail.com>
2022-09-26 08:51:53 +0100

    Add function lookup
    Test for the function is located at tests/0-main.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pyhton3
+# 0-lookup.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module provides a function to list available attributes
+and methods in a class
+"""
+
+def lookup(obj):
+    """List available attributes and methods in an object
+
+    Args:
+        obj (object): object to lookup
+
+    Return:
+        List of attributes and methods
+    """
+    res = []
+    if isinstance(obj, object):
+        res = dir(obj)
+    return res
