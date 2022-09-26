cf57a5890f421b03fdcb1ecdecf805cb2b314d54
Stephen Oba <obastepheno@gmail.com>
2022-09-26 15:03:17 +0100

    Update method str in subclass

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# 10-square.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module provides definition of a Square class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class representation of a Square
+    """
+    def __init__(self, size):
+        """Initialize a Square object
+
+        Args:
+            size (int): width and height of square
+
+        Return:
+            None
+        """
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Computes the Area of Square
+
+        Return:
+            None
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """String representation of square
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
