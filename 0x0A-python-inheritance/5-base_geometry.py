06ecb92949e283247b5b339c6380309b0b339582
Stephen Oba <obastepheno@gmail.com>
2022-09-26 11:35:08 +0100

    Add definition of BaseGeometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module contains defintion of class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry operations
+    """
