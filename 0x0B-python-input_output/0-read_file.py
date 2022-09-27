b6cc6630d5ea0d9f8581c5b43346bfdcd91fd8aa
Stephen Oba <obastepheno@gmail.com>
2022-09-27 22:12:58 +0100

    Update function read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -14,7 +14,6 @@ def read_file(filename=""):
     Return:
         None
     """
-    if filename:
-        with open(filename, encoding="utf-8") as f:
-            for line in f:
-                print(line.strip())
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line.strip())
