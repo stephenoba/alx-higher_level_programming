3e5a716f55df9e3b9f998c6f4ebce3d51b78f749
Stephen Oba <obastepheno@gmail.com>
2022-09-27 22:11:50 +0100

    Add function read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 0-read_file.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module defines a function that reads a file
+"""
+
+
+def read_file(filename=""):
+    """Reads a file
+
+    Args:
+        filename (str): relative path to file
+
+    Return:
+        None
+    """
+    if filename:
+        with open(filename, encoding="utf-8") as f:
+            for line in f:
+                print(line.strip())
