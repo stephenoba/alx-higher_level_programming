3125eb438bebb58379d38332cb09bbdffcece994
Stephen Oba <obastepheno@gmail.com>
2022-09-27 22:51:55 +0100

    Add function to_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 3-to_json_string.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module defines function to convert object to JSON string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Convert object to JSO string
+
+    Args;
+        my_obj (Any): Object
+
+    Return:
+        Json representation of my_obj
+    """
+    string = json.loads(my_obj)
+    return string
