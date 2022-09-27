73d44724ddaa9ee5bc2efcb79adced32d1c80ea7
Stephen Oba <obastepheno@gmail.com>
2022-09-27 22:32:15 +0100

    Add file write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 1-write_file.py
+# Stephen Oba <obastepheno@gmail.com>
+"""Module defines a function to write to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """Write to a file
+
+    Args:
+        filename (str): Path to file
+        text (str): text to write to file
+
+    Return:
+        None
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
