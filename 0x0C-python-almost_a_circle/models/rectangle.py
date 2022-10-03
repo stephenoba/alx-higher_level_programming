fcf9a85a7a83191494e2885c53a42c5fa2d4dbea
Stephen Oba <obastepheno@gmail.com>
2022-10-03 20:28:04 +0100

    Add validations on properties

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,6 +44,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter for width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 1:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -54,6 +58,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter for heiight"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 1:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -64,6 +72,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setter for x position"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -74,4 +86,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter for y position"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
