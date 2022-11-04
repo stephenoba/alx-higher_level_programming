9900d7ba4857ed0f3dfa6ad40a623736bc409dee
Stephen Oba <obastepheno@gmail.com>
2022-11-04 21:49:53 +0100

    Remove NOT NULL constraint

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -4,5 +4,5 @@
 --        id INT with the default value 1
 --        name VARCHAR(256)
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT NOT NULL DEFAULT 1,
+	id INT DEFAULT 1,
 	name VARCHAR(256));
