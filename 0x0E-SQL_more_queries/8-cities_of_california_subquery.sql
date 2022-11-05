917b942b1569145bc0b99397a5d0e2c72f4cd314
Stephen Oba <obastepheno@gmail.com>
2022-11-05 12:54:46 +0100

    exclude state_id column

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of California that can
 -- be found in the database hbtn_0d_usa
-SELECT 	id, state_id, name
+SELECT 	id, name
 FROM	cities c
 WHERE	(SELECT id FROM states
 	WHERE name = "California") = c.state_id
