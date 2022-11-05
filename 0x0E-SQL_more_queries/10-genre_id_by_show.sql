2ec4fde09b7d19b01959fb75d8bff7a4e42c55dd
Stephen Oba <obastepheno@gmail.com>
2022-11-05 19:43:07 +0100

    Add file 10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows that have
+-- at least one genre linked
+SELECT		ts.title, tsg.genre_id
+FROM		tv_shows AS ts
+INNER JOIN	tv_show_genres AS tsg
+ON		(ts.id = tsg.show_id)
+ORDER BY	ts.title, tsg.genre_id;
