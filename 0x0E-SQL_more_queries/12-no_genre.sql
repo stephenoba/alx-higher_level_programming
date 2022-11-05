c272b2dcb95984814e6263ce5196e2b5185ba242
Stephen Oba <obastepheno@gmail.com>
2022-11-05 19:53:03 +0100

    Add file 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- ists all shows contained in hbtn_0d_tvshows without
+-- a genre linked. 
+SELECT		ts.title, tsg.genre_id
+FROM		tv_shows AS ts
+LEFT JOIN	tv_show_genres AS tsg
+ON		(ts.id = tsg.show_id)
+WHERE		tsg.genre_id IS NULL
+ORDER BY	ts.title, tsg.genre_id;
