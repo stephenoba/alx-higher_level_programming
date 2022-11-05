0ed219755ee1ddb5f49ef2115023b0ddb460474b
Stephe Oba <obastepheno@gmail.com>
2022-11-05 15:38:22 +0100

    Updaye sql command

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT		cities.id  cities.name, states.name
 FROM		cities
-INNER JOIN 	states
-ON 		states.id
-WHERE 		states.id = cities state_id;
+LEFT JOIN 	states
+ON 		(states.id = cities.state_id);
