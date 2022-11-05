3823514b6ed6d79db58a8c92271891caef730e7c
Stephen Oba <obastepheno@gmail.com>
2022-11-05 20:24:55 +0100

    Add order by clause

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,6 +6,6 @@ INNER JOIN 	tv_show_genres
 ON 		(tv_genres.id = tv_show_genres.genre_id)
 INNER JOIN 	tv_shows 
 ON (
-	tv_show_genres.show_id = tv_shows.id
-	AND tv_shows.title = "Dexter"
-);
+		tv_show_genres.show_id = tv_shows.id
+		AND tv_shows.title = "Dexter")
+ORDER BY	name;
