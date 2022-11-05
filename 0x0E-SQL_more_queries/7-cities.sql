a261a32b0e77fb493b430d3aaa54c7dc3e86013f
Stephe Oba <obastepheno@gmail.com>
2022-11-05 10:22:29 +0100

    ipfatr sql syntax

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- creates the database hbtn_0d_usa and the table cities
-CREATE TABLE IF NOT EXISTS cities (
-	id INT AUTO_INCREMENT,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY state_id
-		REFERENCES state(id),
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	`id` INT AUTO_INCREMENT,
+	`state_id`	INT		NOT NULL,
+	`name`		VARCHAR(256) 	NOT NULL,
+	PRIMARY KEY(`id`),
+	FOREIGN KEY `state_id`
+		REFERENCES `hbtn_0d_usa`.`state`(`id`)
 	);
