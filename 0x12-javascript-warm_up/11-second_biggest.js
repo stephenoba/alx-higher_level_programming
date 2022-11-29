34fe55554d64def55e5db6ab8df5002ad8e096bd
Stephen Oba <obastepheno@gmail.com>
2022-11-29 20:14:19 +0100

    Fix errored output
    Error was caused due to the setbacks in javascript's sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const numbers = args.slice(2);
+let numbers = args.slice(2);
+
+const convElements = (ele) => {
+  let res = Number(ele);
+  if (!res) {
+    res = 0;
+  }
+  return res;
+}
 
 if (args.length > 2) {
-  numbers.sort().reverse();
+  numbers = numbers.map(convElements);
+  numbers.sort((a, b) => b - a);
   console.log(Number(numbers[1]));
 } else {
   console.log(0);
