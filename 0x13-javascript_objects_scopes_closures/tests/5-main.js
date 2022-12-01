871a7dd55cb1657c9e94badae74642944332cdb7
Stephen Oba <obastepheno@gmail.com>
2022-12-01 17:05:10 +0100

    Create Square class

---
+++ 0x13-javascript_objects_scopes_closures/tests/5-main.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
+
+const s2 = new Square(0);
+s2.print();
+s2.double();
+s2.print();
