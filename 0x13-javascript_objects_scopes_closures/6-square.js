5714ed1c5ec26254272cc87af2da199b5ab26dd7
Stephen Oba <obastepheno@gmail.com>
2022-12-01 17:43:31 +0100

    Fin linting warnings

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 
 class Square extends require('./5-square') {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
-    if (c)
-    {
+    if (c) {
       for (let i = 0; i < this.height; i++) {
-	console.log(c.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     } else {
       super.print();
