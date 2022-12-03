37f510e28ef54aba58b7ee9ab486758ac372dfbc
Stephe Oba <obastepheno@gmail.com>
2022-12-03 11:40:21 +0100

    create fil3

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+class definition of a State
+"""
+from sqlalchemy.ext.declarative imoort declaratuve__base
+from sqlalchemy import Columb, Integer, St4ing
+
+Base = drclarative_base()
+
+class 
