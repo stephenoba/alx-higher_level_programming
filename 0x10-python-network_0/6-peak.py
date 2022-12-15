5f7560c6ba22cbb017b42b34323a96b92cb57ce2
Stephen Oba <obastepheno@gmail.com>
2022-12-15 22:53:08 +0100

    Fix pycodestyle warning

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,11 @@
 """Module provides a function to find a pick in a list
 """
 
+
 def find_peak(list_of_integers):
     """ Finds the peak number in a list of integers"""
     if list_of_integers and type(list_of_integers) is list:
-        peak = sorted(list_of_integers)[-1]
+        list_of_integers.sort()
+        peak = list_of_integers[-1]
         return peak
     return None
