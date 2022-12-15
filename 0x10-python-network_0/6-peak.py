33aed0f0006d5b661a0e0fd539c1fa358cdd7d1f
Stephen Oba <obastepheno@gmail.com>
2022-12-15 22:58:24 +0100

    refractor solution

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,5 +6,7 @@
 def find_peak(list_of_integers):
     """ Finds the peak number in a list of integers"""
     if list_of_integers and type(list_of_integers) is list:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        peak = list_of_integers[-1]
+        return peak
     return None
