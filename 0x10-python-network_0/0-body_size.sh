960dfb55f21c2d133afb0a6f2c036d5a295367a9
Stephen Oba <obastepheno@gmail.com>
2022-12-15 21:43:52 +0100

    Fix error with requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
-# script takes a URL as an argument, 
-# sends a request to that URL,
-# and displays the size of the body of the response
-
+# script prints size in bytes, 
 curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
-# script takes a URL,
-# sends a GET request to the URL,
-# and displays the body of the response
-
+# script prints body of http response,
 curl -sL "$1"
