21bef67bac62f9555e6b95cf859923a1a1689da9
Stephen Oba <obastepheno@gmail.com>
2022-12-19 12:23:05 +0100

    Access response header with requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script gget the 'X-Request-Id' for response header
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    res = requests.get(url)
+    print(res.headers["X-Request-Id"])
